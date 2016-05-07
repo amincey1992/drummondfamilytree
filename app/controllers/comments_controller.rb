351c14eb8c6bcbb29f1cf88a0fdfaadc1458cc54
alex <amincey1992@gmail.com>
2016-05-07 02:30:38 -0400

    init

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -69,6 +69,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:author, :comment_entry, :sell_id, :forum_id, :user_id, :user_name, :avatar)
+      params.require(:comment).permit(:author, :comment_entry, :sell_id, :forum_id, :user_id, :user_name)
     end
 end
