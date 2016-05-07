a7287f1233e33b430be0f9fc3fb09311c7bc9cb2
alex <amincey1992@gmail.com>
2016-05-06 22:45:18 -0400

    init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :weather
+
   def configure_permitted_parameters
    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation, :user_name, :avatar) }
 
@@ -15,13 +15,7 @@ class ApplicationController < ActionController::Base
 
   end
 
-  # def weather 
-  # 	response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/geolookup/conditions/q/GA/Atlanta.json")
-  
-  # @location = response['location'] ? response['location']['city'] : nil
-  # @temp_f = response['current_observation'] ? ['temp_f'] : nil
-  # @weather_icon = response['current_observation'] ? ['icon_url'] : nil
-  # end
+
 end
 
  
