5e87e30e7373a4a9749c76840be8b023f85317dd
alex <amincey1992@gmail.com>
2016-05-07 17:10:56 -0400

    init

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ActiveRecord::Base
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :storage => :s3,  :bucket => 'atlanta-genesis-coupe-club', :s3_credentials => "#{Rails.root}/config/aws.yml", :path => "resources/:id/:style/:basename.:extension"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
