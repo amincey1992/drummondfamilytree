07600927d4639aed28cd94737a66e215f69e3564
alex <amincey1992@gmail.com>
2016-05-07 17:15:32 -0400

    init

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -3,7 +3,7 @@ class Forum < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :storage => :s3,  :bucket => 'atlanta-genesis-coupe-club', :s3_credentials => "#{Rails.root}/config/aws.yml", :path => "resources/:id/:style/:basename.:extension"
+has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :storage => :s3,  :bucket => 'atlanta-genesis-coupe-club', :s3_credentials => "#{Rails.root}/config/environments/production.rb", :path => "resources/:id/:style/:basename.:extension"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   
