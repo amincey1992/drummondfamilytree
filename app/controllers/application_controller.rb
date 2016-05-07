abb65075e5b5eeed4ae78c0006729b0301eac272
alex <amincey1992@gmail.com>
2016-05-06 22:15:38 -0400

    init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,9 +18,9 @@ class ApplicationController < ActionController::Base
   def weather 
   	response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/geolookup/conditions/q/GA/Atlanta.json")
   
-  @location = response['location'] response['location']['city'] 
-  @temp_f = response['current_observation'] ['temp_f'] 
-  @weather_icon = response['current_observation'] ['icon_url'] 
+  @location = response['location'] ? response['location']['city'] : nil
+  @temp_f = response['current_observation'] ? ['temp_f'] : nil
+  @weather_icon = response['current_observation'] ? ['icon_url'] : nil
   end
 end
 
