98e5af896a6ec24ff9f7a2cf5cc9f3c33e0c505c
alex <amincey1992@gmail.com>
2016-05-07 17:43:04 -0400

    init

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ActiveRecord::Base
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", s3_permissions: 'public-read'
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
