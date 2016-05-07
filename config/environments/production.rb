c93be32e795ea84d5f85453d33609ed728596819
alex <amincey1992@gmail.com>
2016-05-06 23:00:58 -0400

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,9 +21,6 @@ config.consider_all_requests_local = true
 
 
 
-
-config.serve_static_assets = true
-config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 config.assets.compile = true
 
 
