e12a2730433a6cad42e76718d1bd47454f1c3e9c
alex <amincey1992@gmail.com>
2016-05-07 18:23:00 -0400

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,8 +93,9 @@ config.assets.precompile += %w( .svg .eot .woff .ttf .png .jpg .gif)
 
 config.paperclip_defaults = {
   :storage => :s3,
+  :region => ENV['AWS_REGION'],
   :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
+    :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
 
