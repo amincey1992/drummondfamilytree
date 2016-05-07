0654d20ebc2d5054d6f728a878452742da597c10
alex <amincey1992@gmail.com>
2016-05-07 16:25:39 -0400

    init

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160502192129) do
+ActiveRecord::Schema.define(version: 20160507180952) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string   "author"
     t.text     "comment_entry"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
-    t.integer  "sell_id"
     t.integer  "user_id"
     t.integer  "forum_id"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.integer  "sell_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -81,18 +84,21 @@ ActiveRecord::Schema.define(version: 20160502192129) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "user_name"
-    t.string   "avatar"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "welcomes", force: :cascade do |t|
     t.datetime "created_at", null: false
