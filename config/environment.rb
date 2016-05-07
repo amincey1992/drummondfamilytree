666b4a0172ed8aad897b06f2bc724e0fa4001624
alex <amincey1992@gmail.com>
2016-05-07 02:32:20 -0400

    init

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-config.gem 'cloudinary'
+
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
-  }
-}
 
