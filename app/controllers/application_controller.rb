2bd05c82b2ababa0c50fb85dcab92be8e6f79534
alex <amincey1992@gmail.com>
2016-05-11 10:27:21 -0400

    omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,11 +16,11 @@ class ApplicationController < ActionController::Base
 
 
  
-  private
+  # private
  
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
 
 end
 
