fdb4c4b6d44bb0e9a13085780453708c3ced1208
alex <amincey1992@gmail.com>
2016-07-12 16:40:59 -0400

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711010218) do
+ActiveRecord::Schema.define(version: 20160712203642) do
 
   create_table "albumeights", force: :cascade do |t|
     t.string   "title"
@@ -213,6 +213,7 @@ ActiveRecord::Schema.define(version: 20160711010218) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
