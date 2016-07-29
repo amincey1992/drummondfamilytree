c126198f15f664ee63dd39113228a3a2a610e2b8
alex <amincey1992@gmail.com>
2016-07-29 12:48:17 -0500

    first commit

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -3,7 +3,8 @@ class Sell < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
-has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :convert_options => { :all => '-auto-orient' }, :default_url => "/images/:style/missing.png"
+
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 acts_as_votable
 def score
