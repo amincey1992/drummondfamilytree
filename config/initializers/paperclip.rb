c39e785b6ea097c45f98b2bdce9794c5f9ecf2c2
alex <amincey1992@gmail.com>
2016-07-29 13:50:47 -0500

    first commit

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,6 @@
 require "paperclip/railtie"
 Paperclip::Railtie.insert
+
+Paperclip::Attachment.default_options[:styles]          = lambda { |attachment| 
+  attachment.content_type.index("image/") == 0 ? {original: {convert_options: "-auto-orient"}} : {}
+}
