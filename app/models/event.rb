4f06601382569dbd8986fc32785fa95a099c1754
alex <amincey1992@gmail.com>
2016-07-29 12:44:29 -0500

    first commit

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	:convert_options => { :all => '-auto-orient' }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
