fda0b648c4a161e1f039e9f3bec152095e9d2e9f
alex <amincey1992@gmail.com>
2016-08-10 22:41:55 -0400

    welcome update

--- app/models/albumten.rb
+++ app/models/albumten.rb
@@ -1,5 +1,5 @@
 class Albumten < ActiveRecord::Base
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 belongs_to :user
 acts_as_votable
